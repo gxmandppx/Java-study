39f3ef5997ba0d8e281b78c71d80558c54ee6542
gxmandppx <2016330070@qq.com>
2021-10-14 12:05:06 +0800

    添加排序

--- JavaTest/src/com/mengying/mainTest.java
+++ JavaTest/src/com/mengying/mainTest.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class mainTest {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        int a=6;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+       /* Scanner sc=new Scanner(System.in);
         int m=sc.nextInt();
         int n=sc.nextInt();
         gdb g=new gdb();
         System.out.println("最大公约数为："+g.g(m,n));
-        System.out.println("最小公倍数为："+m*n/g.g(m,n));
+        System.out.println("最小公倍数为："+m*n/g.g(m,n));*/
     }
 }
